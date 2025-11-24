3dfbfc34d08ee62cc30179cfea17e10b3319c571
Fayro1999 <anosikesomadina8@gmail.com>
2025-11-24 16:31:05 +0100

    Updated project settings to monieplug and other fixes

--- monieplug/wsgi.py
+++ monieplug/wsgi.py
@@ -10,6 +10,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import dotenv
+dotenv.load_dotenv()
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monieplug.settings')
 
