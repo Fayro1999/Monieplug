34ce2ed4cc41a75246b0554f78b14a137e501419
Fayro1999 <anosikesomadina8@gmail.com>
2025-12-29 08:08:16 +0100

    updated settings

--- monieplug/settings.py
+++ monieplug/settings.py
@@ -157,9 +157,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
+EMAIL_PORT = 587
+EMAIL_USE_SSL =  False #True
+EMAIL_USE_TLS = True #False
 EMAIL_HOST_USER = 'monieplug.eservice@gmail.com'
 EMAIL_HOST_PASSWORD = 'wwpi erqo nyla ncda'  # App Password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
