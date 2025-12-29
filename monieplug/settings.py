22f19f15fb10a70c60a1a74c74d48f65c6d462af
Fayro1999 <anosikesomadina8@gmail.com>
2025-12-29 13:16:57 +0100

    updated settings

--- monieplug/settings.py
+++ monieplug/settings.py
@@ -29,8 +29,12 @@ SECRET_KEY = 'django-insecure-l$!1oa0q%a!3%^prsqm*xbc0h*w&5td!ntp16oy_s&ptre!25$
 DEBUG = True
 
 
-ALLOWED_HOSTS = ["monieplug.onrender.com", "127.0.0.1",
-    "localhost"]
+ALLOWED_HOSTS = [
+    "monieplug.onrender.com", "127.0.0.1",
+    "localhost",
+    "https://money-plug-wwiq.vercel.app"
+    
+    ]
 
 
 # Application definition
